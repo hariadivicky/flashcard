b9cd8afa90ec686553cc87a7f8e25ff2c84f0190
Vicky Hariadi <hariadivicky@gmail.com>
2022-11-01 02:02:32 +0700

    add margin & fix indentation

--- src/Layout/Form.js
+++ src/Layout/Form.js
@@ -1,61 +1,61 @@
 import React from 'react'
 
 function Form(props) {
-    const {
-        onSubmit,
-        onFormStateChange,
-        onFormStateFileChange,
-        onCancel,
-        formData,
-        editMode
-    } = props
+  const {
+    onSubmit,
+    onFormStateChange,
+    onFormStateFileChange,
+    onCancel,
+    formData,
+      editMode
+  } = props
 
-    return (
-        <form onSubmit={onSubmit}>
-            <div className= "form-group">
-                <label className="form-label" htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Deck Name"
-                    required
-                    name="name"
-                    onChange={onFormStateChange}
-                    value={formData.name}
-                />
-            </div>
-            <div className="form-group">
-                <label className="form-label" htmlFor="description">Description</label>
-                <textarea
-                    type="text"
-                    className="form-control input-lg"
-                    id="description"
-                    placeholder="Brief description of the deck"
-                    required
-                    name="description"
-                    onChange={onFormStateChange}
-                    value={formData.description}
-                />
-            </div>
+  return (
+    <form onSubmit={onSubmit}>
+      <div className= "form-group">
+        <label className="form-label" htmlFor="name">Name</label>
+        <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Deck Name"
+            required
+            name="name"
+            onChange={onFormStateChange}
+            value={formData.name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="description">Description</label>
+        <textarea
+            type="text"
+            className="form-control input-lg"
+            id="description"
+            placeholder="Brief description of the deck"
+            required
+            name="description"
+            onChange={onFormStateChange}
+            value={formData.description}
+        />
+      </div>
 
-            {!editMode && (
-                <div className="form-group">
-                    <label className="form-label" htmlFor="description">Cards data (optional)</label>
-                    <input
-                        type="file"
-                        className="form-control input-lg"
-                        id="cards"
-                        name="cards"
-                        onChange={onFormStateFileChange}
-                    />
-                </div>
-            )}
+      {!editMode && (
+        <div className="form-group">
+          <label className="form-label" htmlFor="description">Cards data (optional)</label>
+          <input
+            type="file"
+            className="form-control input-lg"
+            id="cards"
+            name="cards"
+            onChange={onFormStateFileChange}
+          />
+        </div>
+      )}
 
-            <button className="btn btn-secondary" onClick={onCancel}>Cancel</button>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    )
+      <button className="btn btn-secondary mr-2" onClick={onCancel}>Cancel</button>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
 }
 
 export default Form;
