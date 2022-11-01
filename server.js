4af47886dee54ce6498e08a0c636faedf1c4730f
Vicky Hariadi <hariadivicky@gmail.com>
2022-11-02 05:53:47 +0700

    disable db creation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { signIn } = require('./api/signIn')
 const dbPath = path.join(__dirname, 'data/db.json')
 
 // create an empty db if it doesn't exists.
-fs.closeSync(fs.openSync(dbPath, 'a'))
+// fs.closeSync(fs.openSync(dbPath, 'a'))
 const adapter = new FileSync(dbPath)
 const db = new low(adapter)
 // database default collections.
